eed914ff666e96cef5ba7b7fca741b26346bb55b
kazukimurahashi12 <unchain1ed@gmail.com>
2026-04-10 23:41:10 +0900

    Create RagProperties.kt

---
+++ src/main/kotlin/com/example/llmragplatform/config/RagProperties.kt
@@ -0,0 +1,11 @@
+package com.example.llmragplatform.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "rag")
+data class RagProperties(
+    val topK: Int = 3,
+    val vectorSearchEnabled: Boolean = false,
+    val embeddingModel: String = "text-embedding-3-small",
+    val embeddingDimensions: Int = 1536,
+)
